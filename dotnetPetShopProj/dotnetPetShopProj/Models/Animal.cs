c4c62a41a0c7e4abd5dc4feae6a1907f98fc668a
GalMashi <112006100+GalMashi@users.noreply.github.com>
2022-08-31 23:44:07 +0300

    changed age to string
    in order to include animals with lifespans shorter than 1 year

--- dotnetPetShopProj/dotnetPetShopProj/Models/Animal.cs
+++ dotnetPetShopProj/dotnetPetShopProj/Models/Animal.cs
@@ -7,7 +7,7 @@ namespace dotnetPetShopProj.Models
         public int AnimalId { get; set; }
         public int CategoryId { get; set; }
         public string? AnimalName { get; set; }
-        public int Age { get; set; }
+        public string? Age { get; set; }
         public string? PictureName { get; set; }
         public string? Description { get; set; }
         public int CommentAmount { get; set; }
